046fc890ceb5507f5b408847c33b2ec2f3fb4257
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2024-08-27 12:19:35 +0530

    header

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your favourite food here</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac urna ut sem aliquam consectetur. Integer leo sapien,</p>
+                <button>View More</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("home")
     return (
         <div className='navbar'>
             <img src={assets.logo} alt="" className='logo' />
 
             <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Mobile-app</li>
-                <li>Contact us</li>
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-app</li>
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact us</li>
             </ul>
 
             <div className="navbar-right">
@@ -22,8 +23,6 @@ const Navbar = () => {
                 </div>
                 <button>sign</button>
             </div>
-
-            <h1>navbar</h1>
         </div>
     )
 }
