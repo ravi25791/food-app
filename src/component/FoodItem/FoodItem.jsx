f6bfd3a1511942bbf7ad55cb318a8cc058b07224
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2024-08-28 12:53:04 +0530

    food display

---
+++ src/component/FoodItem/FoodItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './foodItem.css'
+
+const FoodItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FoodItem
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../component/Header/Header'
 import './home.css'
+import ExploreMenu from '../../component/ExplorMenu/ExploreMenu'
+import FoodDisplay from '../../component/FoodDisplay/FoodDisplay'
+
 
 const Home = () => {
+    const [category, setCategory] = useState("");
     return (
         <div>
             <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
+            <FoodDisplay category={category} />
         </div>
     )
 }
